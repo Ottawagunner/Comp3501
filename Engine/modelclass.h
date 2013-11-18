f16224dd098c1e28fd058b1bfe5064aa87cbe0e7
Ben Black <benjyblack@gmail.com>
2013-11-18 11:07:28 -0500

    Framework from Rastertek Terrain Tutorial #8 w/ Model Loader adapted from Rastertek DirectX11 Tutorial #8. TODO: Add modelshaderclass and render a cube to the screen. Also included a "jeep" model and textures we could potentially use

---
+++ Engine/modelclass.h
@@ -0,0 +1,84 @@
+#pragma once
+
+////////////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _MODELCLASS_H_
+#define _MODELCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <d3dx10math.h>
+#include <fstream>
+using namespace std;
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "textureclass.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ModelClass
+////////////////////////////////////////////////////////////////////////////////
+class ModelClass
+{
+private:
+	struct InputType
+	{
+		float x, y, z;
+	};
+	
+	struct FaceType
+	{
+		int vIndex1, vIndex2, vIndex3;
+		int tIndex1, tIndex2, tIndex3;
+		int nIndex1, nIndex2, nIndex3;
+	};
+
+	struct VertexType
+	{
+		D3DXVECTOR3 position;
+	    D3DXVECTOR2 texture;
+		D3DXVECTOR3 normal;
+	};
+
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass&);
+	~ModelClass();
+
+	bool Initialize(ID3D11Device*, char*, WCHAR*);
+	void Shutdown();
+	void Render(ID3D11DeviceContext*);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView* GetTexture();
+
+	bool LoadModel(char* filename);
+
+private:
+	bool InitializeBuffers(ID3D11Device*);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext*);
+
+	bool LoadTexture(ID3D11Device*, WCHAR*);
+	void ReleaseTexture();
+	
+	bool ReadFileCounts(char* filename, int&, int&, int&, int&);
+
+private:
+	ID3D11Buffer*		m_vertexBuffer, *m_indexBuffer;
+	int					m_vertexCount, m_indexCount;
+	TextureClass*		m_Texture;
+
+	VertexType*			m_vertices;
+	unsigned long*		m_indices;
+};
+
+#endif
